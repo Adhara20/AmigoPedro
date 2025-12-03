9793072aeea8c176d9c998765a40cf9ef66de53e
Sofia-Osuna <sofiaosuna224@gmail.com>
2025-12-03 09:00:49 -0700

    form de producto

--- registroUsuario.php
+++ registroUsuario.php
@@ -8,6 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro</title>
     <link href="src/output.css" rel="stylesheet">
+    <script src="https://cdn.tailwindcss.com"></script>   
 </head>
 
 <body class="bg-cover bg-no-repeat bg-center min-h-screen"
